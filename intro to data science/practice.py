529af796003f4ce7bc3a255c528d566ca2adfaaf
Steven Fuller <65366364+sfulle14@users.noreply.github.com>
2021-02-05 09:10:51 -0600

    Add files via upload

---
+++ intro to data science/practice.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+sports = {'Archery': 'Bhutan',
+          'Golf': 'Scotland',
+          'Sumo': 'Japan',
+          'Taekwondo': 'South Korea'}
+s = pd.Series(sports)
+s
+s.iloc[3]
